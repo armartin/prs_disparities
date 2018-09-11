2ebcdf4a18bedc7cd310dec3ba61703bca06c581
armartin <armartin@stanford.edu>
2018-09-10 21:32:29 -0400

    fixed a bug (rarely used a wrong pheno column)

--- holdout_ukbb_bbj_r2.R
+++ holdout_ukbb_bbj_r2.R
@@ -6,7 +6,13 @@ library(doParallel)
 cl = makeCluster(8, 'PSOCK')
 registerDoParallel(cl)
 
-setwd('/Users/alicia/daly_lab/manuscripts/knowles_ashley_response/analysis/')
+afr=FALSE
+
+if(afr) {
+  setwd('/Users/alicia/daly_lab/manuscripts/knowles_ashley_response/analysis/ukbb_afr/')
+} else {
+  setwd('/Users/alicia/daly_lab/manuscripts/knowles_ashley_response/analysis/')
+}
 
 covariates = c("isFemale", "age", "age_squared", "age_isFemale", "age_squared_isFemale", paste0("PC", seq(20)))
 
@@ -22,7 +28,7 @@ bootstrap_CIs <- function(my_subset) {
 # Get R^2 and p-value for nested model
 compute_r2 <- function(dataset, phenotype, p) {
   print(paste(phenotype, p))
-  my_subset <- subset(dataset, p_threshold==p & pheno==phenotype)
+  my_subset <- subset(dataset, p_threshold==p & pheno_name==phenotype)
   model1 <- lm(as.formula(sprintf("true_pheno ~ PRS + %s", paste(covariates, collapse = " + "))), data=my_subset)
   model0 <- lm(as.formula(sprintf("true_pheno ~ %s", paste(covariates, collapse = " + "))), data=my_subset)
   r2 <- summary(model1)$adj.r.squared - summary(model0)$adj.r.squared
@@ -31,32 +37,43 @@ compute_r2 <- function(dataset, phenotype, p) {
   delta <- bs_means - r2
   d = quantile(delta, c(0.025, 0.975))
   ci = r2 - c(d[1], d[2])
-  r2_vec <- c(r2, rev(ci), pval, my_subset$pheno[1], my_subset$p_threshold[1])
+  r2_vec <- c(r2, rev(ci), pval, my_subset$pheno_name[1], my_subset$p_threshold[1])
   names(r2_vec) <- c('r2', '2.5%', '97.5%', 'p', 'pheno', 'p_threshold')
   return(r2_vec)
 }
 
 # Read in and wrangle PRS + phenotype + covariate data
-read_phenos <- function(pheno_name) {
-  my_pheno <- read.table(paste0('ukbb/UKB_', pheno_name, '_PRS.txt.bgz'), header=T)
-  pheno <- colnames(my_pheno)[27]
+read_phenos <- function(pheno_name, afr) {
+  if(afr) {
+    my_pheno <- read.table(paste0('ukbb/UKB_', pheno_name, '_afr_PRS.txt.bgz'), header=T)  
+    afr_subset <- read.table('../ukb31063_afr_subset_samples.tsv', header=T)
+    afr_subset <- subset(afr_subset, afr_select)
+    my_pheno <- subset(my_pheno, s %in% afr_subset$s)
+  } else {
+    my_pheno <- read.table(paste0('ukbb/UKB_', pheno_name, '_PRS.txt.bgz'), header=T)
+  }
+  my_pheno <- my_pheno %>%
+    select(s:PC20,pheno_name,s1:s5)
   colnames(my_pheno)[27] <- 'true_pheno'
   my_pheno <- my_pheno %>%
     gather('p_threshold', 'PRS', num_range('s', 1:10))
-  my_pheno$pheno <- pheno_name
+  my_pheno$pheno_name <- pheno_name
+  # my_pheno$pheno <- pheno_name
   return(my_pheno)
 }
 
 phenos <- c('height', 'bmi', 'sbp', 'dbp', 'wbc', 'monocyte', 'neutrophil', 'eosinophil', 'basophil', 'lymphocyte',
             'rbc', 'mch', 'mcv', 'mchc', 'hb', 'ht', 'plt')
 
-date(); all_phenos <- ldply(phenos, read_phenos, .parallel=T, .paropts=list(.packages=c('dplyr', 'tidyr'))); date()
 
-p_thresholds <- paste0('s', 1:10)
+date(); all_phenos <- ldply(phenos, function(x) { ldply(afr, function(y) {read_phenos(x, y) } ) }); date()
+
+p_thresholds <- paste0('s', 1:5)
 
 # run this for all phenos, takes ~3 minutes per phenotype
 date(); r2_combined <- ldply(phenos, function(x) { ldply(p_thresholds, function(y) { compute_r2(all_phenos, x, y) } ) }, 
-                             .parallel=T, .paropts=list(.export = c("compute_r2", "bootstrap_CIs", "p_thresholds", "all_phenos", "covariates"))); date()
+                             .parallel=T, .paropts=list(.export = c("compute_r2", "bootstrap_CIs", "p_thresholds", "all_phenos", "covariates"),
+                                                        .packages = c('dplyr', 'plyr'))); date()
 
 write.table(r2_combined, 'ukbb_from_ukbb.r2.txt', quote = F, row.names = F, sep = '\t')
 
